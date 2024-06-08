c3d8665e1192f962196bd9c5c7a5c2802875cd47
Eugene Gatawa Makita <73113215+EugeneMakita@users.noreply.github.com>
2024-06-08 14:23:57 -0700

    update sample-loop

--- sample-loop.asm
+++ sample-loop.asm
@@ -8,6 +8,9 @@
     clc ; clear the carry flag 
     sbc #1 ;decrease the value of the accumulator
     dex; decrease the countdown
-    bne CountDown ; redo until equal to zero
+    bne CountDown ; redo until equal to one
+    clc ; clear the carry flag 
+    sbc #1 ;decrease the value of the accumulator
+    sta $00; this loop doesn't cover zero
     rts
 .endproc

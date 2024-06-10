a79d7b88a3d5604491023fda1c6f4e34690a3ef0
Eugene Gatawa Makita <73113215+EugeneMakita@users.noreply.github.com>
2024-06-09 17:27:46 -0700

    update the code

---
+++ 16-bit-sum-of-data.asm
@@ -0,0 +1,36 @@
+.export Main
+.segment "CODE"
+.proc Main
+    ;loading data
+    lda #03
+    sta $42
+
+    lda #$C8
+    sta $43
+
+    lda #$FA
+    sta $44
+
+    lda #$96
+    sta $45
+
+    lda #0
+    tax 
+    tay
+
+    clc
+    Add: 
+        adc $43,X
+        bcs Upper
+        inx
+        cpx $42
+        bne Add
+    sta $40
+    sty $41
+    rts
+
+    Upper:
+        iny
+        jmp Add
+    rts
+.endproc

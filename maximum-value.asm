f5475d0fde134c621f03e258bce26dbcb55fc247
Eugene Gatawa Makita <73113215+EugeneMakita@users.noreply.github.com>
2024-06-11 02:54:07 -0700

    find the biggest number

---
+++ maximum-value.asm
@@ -0,0 +1,39 @@
+.export Main
+.segment "CODE"
+.proc Main
+	;load data
+	lda #$05
+	sta $41
+
+	lda #$67
+	sta $42
+
+	lda #$79
+	sta $43
+
+	lda #$15
+	sta $44
+
+	lda #$E3
+	sta $45
+
+	lda #$72
+	sta $46
+
+	;find the largest number
+	ldx #0
+	lda #0
+
+	Loop:
+		cmp $41,X
+        bcs Smaller
+	lda $41,X 
+	sta $40 
+	Smaller:
+		inx
+		cpx #06
+		bne Loop
+
+
+	rts
+.endproc
